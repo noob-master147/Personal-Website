7ed7b4af9151c40d3bbd0e28d7dec10bf5752c80
noob-master147 <divyanshkhandelwal147@gmail.com>
2019-10-15 01:34:51 +0530

    conflits resolved

--- src/particles.js
+++ src/particles.js
@@ -1,10 +1,6 @@
 window.onload = function() {
     Particles.init({
       selector: '.background',
-<<<<<<< HEAD
       maxParticles: 1000,
-=======
-      maxParticles: 600,
->>>>>>> c28c68dd025e4baecb418f66501cf19d7344cf12
     });
   };
